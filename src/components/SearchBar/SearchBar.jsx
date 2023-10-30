ee7bdce770cb2705561f0583f8022e450072ea18
Chris Kildunne <136514462+kilzridesbikes@users.noreply.github.com>
2023-10-30 15:53:24 -0600

    more styling

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,49 +1,48 @@
-import  { useEffect, useState } from 'react';
-import {  useNavigate } from 'react-router-dom'; 
-import * as productsAPI from "../../utilities/products-api";
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import * as productsAPI from '../../utilities/products-api';
+import './SearchBar.css'; // Import your CSS file for styling
 
-export default function SearchBar(){
-    const [searchInput, setSearchInput] = useState("");
-    const [products, setProducts] = useState([])
-    const navigate = useNavigate()
+export default function SearchBar() {
+  const [searchInput, setSearchInput] = useState('');
+  const [products, setProducts] = useState([]);
+  const navigate = useNavigate();
 
-    useEffect (() => {
-        const getProducts = async () => {
-            const productList = await productsAPI.getAll()
-            setProducts(productList)
-        }
-        getProducts()
-    }, [])
+  useEffect(() => {
+    const getProducts = async () => {
+      const productList = await productsAPI.getAll();
+      setProducts(productList);
+    };
+    getProducts();
+  }, []);
 
-    const handleChange = (e) => {
-        e.preventDefault();
-        setSearchInput(e.target.value);
-      };
-      
-    const handleSearch = () => {
-        const product = products.find((product) => product.name.toLowerCase().includes(searchInput.toLocaleLowerCase()))
-        if(product){
-            setSearchInput("")
-            navigate(`/product/${product._id}`)
-        }else{
-            console.log(product)
-        }
+  const handleChange = (e) => {
+    setSearchInput(e.target.value);
+  };
+
+  const handleSearch = () => {
+    const product = products.find((product) =>
+      product.name.toLowerCase().includes(searchInput.toLowerCase())
+    );
+    if (product) {
+      setSearchInput('');
+      navigate(`/product/${product._id}`);
+    } else {
+      console.log(product);
     }
-    return (
-        <div>
-          <input
-            type="text"
-            placeholder="Search here"
-            onChange={handleChange}
-            value={searchInput}
-          />
-        <button className='btn btn-primary' onClick={handleSearch}>Search</button>
-      </div>
-      );
-    }
-    
-    
-    
-    
-    
-    
+  };
+
+  return (
+    <div className="search-bar">
+      <input
+        type="text"
+        placeholder="Search here"
+        onChange={handleChange}
+        value={searchInput}
+      />
+      <button className="btn btn-primary" onClick={handleSearch}>
+        Search
+      </button>
+    </div>
+  );
+}
