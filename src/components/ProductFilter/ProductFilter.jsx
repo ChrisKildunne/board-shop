1034120538018b6d93ef088e59acbf728b5f93da
Chris Kildunne <136514462+kilzridesbikes@users.noreply.github.com>
2023-10-30 15:45:41 -0600

    filter working

--- src/components/ProductFilter/ProductFilter.jsx
+++ src/components/ProductFilter/ProductFilter.jsx
@@ -1,94 +1,115 @@
 import React, { useState } from "react";
 import './ProductFilter.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
 
 export default function ProductFilter({ handleFilter }) {
-  return (
-    <>
-      <div className="filter-options">
-        <label>Price Range:</label>
-        <div>
-          <input
-            type="checkbox"
-            id="0-100"
-            onChange={() => handleFilter("price", "0-100")}
-          />
-          <label htmlFor="0-100">$0 - $100</label>
-        </div>
-        <div>
-        <input
-            type="checkbox"
-             id="101-200"
-          onChange={() => handleFilter("price", "101-200")}
-        />
-          <label htmlFor="101-200">$101 - $200</label>
-        </div>
-        <div>
-          <input
-            type="checkbox"
-            id="201-300"
-            onChange={() => handleFilter("price", "201-300")}
-          />
-          <label htmlFor="201-300">$201 - $300</label>
-        </div>
-        <div>
-          <input
-            type="checkbox"
-            id="301-400"
-            onChange={() => handleFilter("price", "301-400")}
-          />
-          <label htmlFor="301-400">$301 - $400</label>
-        </div>
-        <div>
-          <input
-            type="checkbox"
-            id="400+"
-            onChange={() => handleFilter("price", "400+")}
-          />
-          <label htmlFor="400+">$400+</label>
-        </div>
+  const [priceVisible, setPriceVisible] = useState(true);
+  const [categoryVisible, setCategoryVisible] = useState(true);
 
-      <label>Category:</label>
-      <div>
-        <input
-          type="checkbox"
-          id="Snowboard"
-          onChange={() => handleFilter("category", "Snowboard")}
-          />
-        <label htmlFor="Snowboard">Snowboard</label>
-      </div>
-      <div>
-        <input
-          type="checkbox"
-          id="Goggles"
-          onChange={() => handleFilter("category", "Goggles")}
-          />
-        <label htmlFor="Goggles">Goggles</label>
-      </div>
-      <div>
-        <input
-          type="checkbox"
-          id="Helmet"
-          onChange={() => handleFilter("category", "Helmet")}
-          />
-        <label htmlFor="Helmet">Helmet</label>
-      </div>
-      <div>
-        <input
-          type="checkbox"
-          id="Bindings"
-          onChange={() => handleFilter("category", "Bindings")}
-          />
-        <label htmlFor="Bindings">Bindings</label>
-      </div>
-      <div>
-        <input
-          type="checkbox"
-          id="Boots"
-          onChange={() => handleFilter("category", "Boots")}
-          />
-        <label htmlFor="Boots">Boots</label>
-      </div>
+  const togglePrice = () => {
+    setPriceVisible(!priceVisible);
+  };
+
+  const toggleCategory = () => {
+    setCategoryVisible(!categoryVisible);
+  };
+
+  return (
+    <div className="filter-options">
+      <div onClick={togglePrice}>
+      <label>Price Range <FontAwesomeIcon icon={faAngleDown} /></label>      </div>
+      {priceVisible && (
+        <>
+          <div>
+            <input
+              type="checkbox"
+              id="0-100"
+              onChange={() => handleFilter("price", "0-100")}
+            />
+            <label htmlFor="0-100">$0 - $100</label>
           </div>
-    </>
+          <div>
+            <input
+              type="checkbox"
+              id="101-200"
+              onChange={() => handleFilter("price", "101-200")}
+            />
+            <label htmlFor="101-200">$101 - $200</label>
+          </div>
+          <div>
+            <input
+              type="checkbox"
+              id="201-300"
+              onChange={() => handleFilter("price", "201-300")}
+            />
+            <label htmlFor="201-300">$201 - $300</label>
+          </div>
+          <div>
+            <input
+              type="checkbox"
+              id="301-400"
+              onChange={() => handleFilter("price", "301-400")}
+            />
+            <label htmlFor="301-400">$301 - $400</label>
+          </div>
+          <div>
+            <input
+              type="checkbox"
+              id="400+"
+              onChange={() => handleFilter("price", "400+")}
+            />
+            <label htmlFor="400+">$400+</label>
+          </div>
+        </>
+      )}
+      <div onClick={toggleCategory}>
+        <label>Category <FontAwesomeIcon icon={faAngleDown} /></label>
+      </div>
+      {categoryVisible && (
+        <>
+          <div>
+            <input
+              type="checkbox"
+              id="Snowboard"
+              onChange={() => handleFilter("category", "Snowboard")}
+            />
+            <label htmlFor="Snowboard">Snowboard</label>
+          </div>
+          <div>
+            <input
+              type="checkbox"
+              id="Goggles"
+              onChange={() => handleFilter("category", "Goggles")}
+            />
+            <label htmlFor="Goggles">Goggles</label>
+          </div>
+          <div>
+            <input
+              type="checkbox"
+              id="Helmet"
+              onChange={() => handleFilter("category", "Helmet")}
+            />
+            <label htmlFor="Helmet">Helmet</label>
+          </div>
+          <div>
+            <input
+              type="checkbox"
+              id="Bindings"
+              onChange={() => handleFilter("category", "Bindings")}
+            />
+            <label htmlFor="Bindings">Bindings</label>
+          </div>
+          <div>
+            <input
+              type="checkbox"
+              id="Boots"
+              onChange={() => handleFilter("category", "Boots")}
+            />
+            <label htmlFor="Boots">Boots</label>
+          </div>
+        </>
+      )}
+    </div>
   );
 }
--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -4,72 +4,69 @@ import "./ProductList.css";
 import ProductFilter from "../ProductFilter/ProductFilter";
 
 export default function ProductList({ productItems, handleAddToCart, user, isPaid }) {
-    const [filters, setFilters] = useState({
-        price: null, // Set a default value
-        brand: [],
-        rating: [],
-        category: [],
-    });
+  const [filters, setFilters] = useState({
+    price: [], // Use an array to store selected price ranges
+    brand: [],
+    rating: [],
+    category: [],
+  });
 
-    const handleFilter = (filterType, value) => {
-        const updatedFilters = { ...filters };
-      
-        if (filterType === 'category') {
-          if (updatedFilters[filterType].includes(value)) {
-            updatedFilters[filterType] = updatedFilters[filterType].filter((cat) => cat !== value);
-          } else {
-            updatedFilters[filterType].push(value);
-          }
-        } else {
-          updatedFilters[filterType] = [value];
-        }
-              setFilters(updatedFilters);
-      };
-      
-      
+  const handleFilter = (filterType, value) => {
+    const updatedFilters = { ...filters };
 
-      const filteredProducts = productItems.filter((product) => {
-        const { price, brand, rating, category } = filters;
-        console.log(price)
-      
-        if (price === "0-100" && product.price >= 0 && product.price <= 100) {
-            return true;
-        } else if (price === "101-200" && product.price > 100 && product.price <= 200) {
-            return true;
-        } else if (price === "201-300" && product.price > 200 && product.price <= 300) {
-            return true;
-        } else if (price === "301-400" && product.price > 300 && product.price <= 400) {
-            return true;
-        } else if (price === "400+" && product.price > 400) {
-            return true;
-        }
-      
-        // Continue to apply filters for brand, rating, and category as before
-      
-        return (
-          (brand.length === 0 || brand.includes(product.brand)) &&
-          (rating.length === 0 || rating.includes(product.rating)) &&
-          (category.length === 0 || category.includes(product.cat))
-        );
-      });
-      
+    if (filterType === 'category') {
+      if (updatedFilters[filterType].includes(value)) {
+        updatedFilters[filterType] = updatedFilters[filterType].filter((cat) => cat !== value);
+      } else {
+        updatedFilters[filterType].push(value);
+      }
+    } else if (filterType === 'price') {
+      // Toggle the selection of price ranges
+      if (updatedFilters[filterType].includes(value)) {
+        updatedFilters[filterType] = updatedFilters[filterType].filter((range) => range !== value);
+      } else {
+        updatedFilters[filterType].push(value);
+      }
+    }
+    // Add a default case for unexpected filter types here if needed.
 
-    const products = filteredProducts.map((product) => (
-        <ProductListItem
-            key={product._id}
-            productItem={product}
-            handleAddToCart={handleAddToCart}
-            user={user}
-        />
-    ));
+    setFilters(updatedFilters);
+  };
 
-    return (
-        <main className="ProductList">
-            <ProductFilter filters={filters} handleFilter={handleFilter} />
-            {products}
-            
+  const filteredProducts = productItems.filter((product) => {
+    const { price, brand, rating, category } = filters;
 
-        </main>
-    );
+    if (price.length === 0) {
+      return (
+        (brand.length === 0 || brand.includes(product.brand)) &&
+        (rating.length === 0 || rating.includes(product.rating)) &&
+        (category.length === 0 || category.includes(product.cat))
+      );
+    } else {
+      // Filter products based on selected price ranges
+      return price.some((selectedRange) => {
+        const [minPrice, maxPrice] = selectedRange.split('-').map(Number);
+        return product.price >= minPrice && product.price <= maxPrice;
+      }) &&
+        (brand.length === 0 || brand.includes(product.brand)) &&
+        (rating.length === 0 || rating.includes(product.rating)) &&
+        (category.length === 0 || category.includes(product.cat));
+    }
+  });
+
+  const products = filteredProducts.map((product) => (
+    <ProductListItem
+      key={product._id}
+      productItem={product}
+      handleAddToCart={handleAddToCart}
+      user={user}
+    />
+  ));
+
+  return (
+    <main className="ProductList">
+      <ProductFilter filters={filters} handleFilter={handleFilter} />
+      {products}
+    </main>
+  );
 }
-
